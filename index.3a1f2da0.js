2c45c03b58351fc52e0399077a675d3a84a5563d
dima <shymdima74@gmail.com>
2024-10-13 12:52:37 +0300

    make build

---
+++ index.3a1f2da0.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall"),t=document.querySelector(".spider"),n=e.getBoundingClientRect().height/2-t.getBoundingClientRect().height/2,i=e.getBoundingClientRect().width/2-t.getBoundingClientRect().width/2;t.style.top="".concat(n,"px"),t.style.left="".concat(i,"px");
+//# sourceMappingURL=index.3a1f2da0.js.map
